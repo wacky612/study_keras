b7e7977fb5f67a926b3d61f3b6dc1687d803bc27
wacky <wacky612@gmail.com>
2021-09-25 14:32:59 +0900

    IMDbデータセット

---
+++ imdb.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+
+import numpy as np
+import matplotlib.pyplot as plt
+from keras import models
+from keras import layers
+from keras.datasets import imdb
+
+def vectorize_sequences(sequences, dimension=10000):
+    results = np.zeros((len(sequences), dimension))
+
+    for i, sequence in enumerate(sequences):
+        results[i, sequence] = 1.
+    
+    return results
+
+(train_data, train_labels), (test_data, test_labels) = imdb.load_data(num_words=10000)
+
+x_train = vectorize_sequences(train_data)
+x_test  = vectorize_sequences(test_data)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test  = np.asarray(test_labels).astype('float32')
+
+model = models.Sequential()
+model.add(layers.Dense(16, activation='relu', input_shape=(10000,)))
+model.add(layers.Dense(16, activation='relu'))
+model.add(layers.Dense( 1, activation='sigmoid'))
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+
+x_val = x_train[:10000]
+partial_x_train = x_train[10000:]
+
+y_val = y_train[:10000]
+partial_y_train = y_train[10000:]
+
+history = model.fit(partial_x_train, partial_y_train, epochs=20, batch_size=512,
+                    validation_data=(x_val, y_val))
+
+history_dict = history.history
+
+loss_values = history_dict['loss']
+val_loss_values = history_dict['val_loss']
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+
+epochs = range(1, len(loss_values) + 1)
+
+fig = plt.figure()
+plt.plot(epochs, loss_values    , 'bo', label='Training loss')
+plt.plot(epochs, val_loss_values, 'b' , label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.savefig('imdb_loss.png')
+
+plt.clf()
+plt.plot(epochs, acc    , 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b' , label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.savefig('imdb_acc.png')
